9d6ae3c15e821021ca6dd4a043aa4ccb2b8ec08f
Oscar Rovira <oscarrovira@me.com>
2018-06-07 13:44:23 +0200

    Actualitzat laravel a 5.6 i instal·lat passport. Petita modificació al seeder per incloure ESO

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,40 +12,41 @@ class DatabaseSeeder extends Seeder {
 	public function run() {
 		// $this->call(UsersTableSeeder::class);
 		// Creem un usuari
-		DB::table ( 'users' )->insert ( [ 
+		DB::table ( 'users' )->insert ( [
 				'name' => 'admin',
 				'email' => 'admin@gmail.com',
-				'password' => bcrypt ( 'admin' ) 
+				'password' => bcrypt ( 'admin' )
 		] );
 		$data = Carbon::create ( 2018, 7, 4 );
 		$array = array (
-				'Bat',
+				'ESO',
+				'Batx',
 				'CAI',
 				'Farmàcia, Jurídics',
 				'Activitats comercials',
 				'SMX',
-				'Gestió administrativa' 
+				'Gestió administrativa'
 		);
 		foreach ( $array as &$valor ) {
-			
-			DB::table ( 'estudis' )->insert ( [ 
-					'nom_estudi' => $valor 
+
+			DB::table ( 'estudis' )->insert ( [
+					'nom_estudi' => $valor
 			] );
 			$estudi = Estudi::where ( 'nom_estudi', $valor )->first ();
-			DB::table ( 'dias' )->insert ( [ 
+			DB::table ( 'dias' )->insert ( [
 					'dia' => $data,
-					'estudi_id' => $estudi->id 
+					'estudi_id' => $estudi->id
 			] );
 			$dia = Dia::orderBy ( 'created_at', 'desc' )->first ();
-			
+
 			$horaInici = Carbon::create ( $dia->year, $dia->month, $dia->day, 9 );
 			$horaFi = Carbon::create ( $dia->year, $dia->month, $dia->day, 13 );
 			while ( $horaInici < $horaFi ) {
 				for($i = 0; $i < 4; $i ++) {
-					DB::table ( 'citas' )->insert ( [ 
+					DB::table ( 'citas' )->insert ( [
 							'hora' => $horaInici->format ( 'H:i' ),
 							'estat' => 'buit',
-							'dia_id' => $dia->id 
+							'dia_id' => $dia->id
 					] );
 				}
 				$horaInici = $horaInici->addMinutes ( 10 );
